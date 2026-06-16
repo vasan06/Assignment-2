2e2a6ad7b91d72ad2ad29057dbba30b75991229d
Vasan <keerthivasan83000@gmail.com>
2026-06-16 10:40:22 +0530

    Delete backend directory

--- backend/app/models/__init__.py
+++
@@ -1,5 +0,0 @@
-from app.models.user import User, UserRole
-from app.models.video import Video, VideoStatus
-from app.models.watch_log import WatchLog, Favorite
-
-__all__ = ["User", "UserRole", "Video", "VideoStatus", "WatchLog", "Favorite"]
--- backend/lambda_handler.py
+++
@@ -1,5 +0,0 @@
-from mangum import Mangum
-
-from app.main import app
-
-handler = Mangum(app)
